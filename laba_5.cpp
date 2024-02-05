0e25ace2e37af709a7741f6ad09fa00fed96fe96
Pavel Gulin <pavelgul1n@yandex.ru>
2024-02-05 20:58:08 +0500

    Added entered first and second numbers

---
+++ laba_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+    // Запрос на ввод первого числа
+    std::cout << "First number: ";
+    std::cin >> num1;
+
+    // Запрос на ввод второго числа
+    std::cout << "Second number: ";
+    std::cin >> num2;
+
+    // Вывод суммы введенных чисел
+    //std::cout << "Sum: " << num1 + num2 << std::endl;
+
+    return 0;
+}
