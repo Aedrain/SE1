a6b2639609243a8f09c11728a43d30e8d3c4989b
Pavel Gulin <pavelgul1n@yandex.ru>
2024-02-08 19:08:12 +0500

    Added Division between numbers

--- laba_5.cpp
+++ laba_5.cpp
@@ -1,4 +1,9 @@
 //training programme
+/*
+Автор - Павел Гулин
+Группа - КЭ-217
+Год - 2024
+*/
 #include <iostream>
 int main() {
     int num1, num2;
@@ -11,8 +16,7 @@ int main() {
     std::cin >> num2;
 
     std::cout << "Sum: " << num1 + num2 << std::endl;
-
     std::cout << "Diff: " << num1 - num2 << std::endl;
-
+    std::cout << "Div: " << num1 / num2 << std::endl;
     return 0;
 }
