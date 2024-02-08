6cbfdb9543eef1839745408ddcc0647e206730de
Pavel Gulin <pavelgul1n@yandex.ru>
2024-02-08 18:08:28 +0500

    Added comment

--- laba_5.cpp
+++ laba_5.cpp
@@ -1,18 +1,18 @@
+//training programme
 #include <iostream>
-
 int main() {
     int num1, num2;
 
-    // Запрос на ввод первого числа
+  
     std::cout << "First number: ";
     std::cin >> num1;
 
-    // Запрос на ввод второго числа
     std::cout << "Second number: ";
     std::cin >> num2;
 
-    // Вывод суммы введенных чисел
     std::cout << "Sum: " << num1 + num2 << std::endl;
+
     std::cout << "Diff: " << num1 - num2 << std::endl;
+
     return 0;
 }
